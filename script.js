43d62f4b03f257103b554d280bff60d97c95772d
Anubhav Saini <anubhav.21b1531122@abes.ac.in>
2024-09-14 00:15:58 +0530

    JS password genrator

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const symbols = '~`!@#$%^&*()_-+={[}]|:;"<,>.?/';
 
 let password="";
 let passwordLength=15;
-let checkCount=1;
+let checkCount=0;
 handleSlider();
 //ste strength circle color to grey
 
@@ -75,7 +75,123 @@ function calcStrength(){
     }
 }
 
-function copyContent(){
-      
+//copyBTn works
+ async function  copyContent(){
+  try{
+    await navigator.clipboard.writeText(passwordDisplay.value);
+    copyMsg.innerText="copied";
+  }
+  catch(e){
+         copyMsg.innerText="failed";
+  }
+  //to make copy wala span visible
+  copyMsg.classList.add("active");
+    //time 
+  setTimeout(() =>{
+    copyMsg.classList.remove("active");
+  },2000);
+}
+
+//shuffle password
+function shufflePassword(array){
+   //Fisher Yates Method
+   for (let i = array.length - 1; i > 0; i--) {
+    //random J, find out using random function
+    const j = Math.floor(Math.random() * (i + 1));
+    //swap number at i index and j index
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+let str = "";
+array.forEach((el) => (str += el));
+return str;
 
 }
+
+//checkbox count everytime call
+function handleCheckBoxChange(){
+  checkCount=0;
+  allCheckBox.forEach((checkbox)=>{
+    if(checkbox.checked)
+      checkCount++;
+  })
+  //special case
+  if(passwordLength<checkCount){
+    passwordLength=checkCount;
+    handleSlider();
+  }
+}
+// call checkbox eventlistner
+allCheckBox.forEach((checkbox)=>{
+  checkbox.addEventListener('change',handleCheckBoxChange);
+})
+
+//slider length change
+inputSlider.addEventListener('input',(e)=>{
+  passwordLength=e.target.value;
+  handleSlider();
+})
+//copy content of input box if value present 
+copyBtn.addEventListener('click',()=>{
+  if(passwordDisplay.value)
+    copyContent();
+})
+generateBtn.addEventListener('click',()=>{
+   //none of the checkbox are selected
+   if(checkCount<=0) return;
+   if(passwordLength<checkCount){
+    passwordLength=checkCount;
+    handleSlider();
+   }
+   //lets start the journey to find new password
+     console.log("starting");
+   //remove old password
+   password="";
+
+   //lets put the stuff mentioned by checkboxes
+  //  if(uppercaseCheck.checked){
+  //   password+=generateUpperCase();
+  //  }
+  //  if(lowercaseCheck.checked){
+  //   password+=generateLowerCase();
+  //  }
+  //  if(numbersCheck.checked){
+  //   password+=generateRandomNumber();
+  //  }
+  //  if(symbolsCheck.checked){
+  //   password+=generateSymbol();
+  //  }
+
+  let funcArr=[];
+  if(uppercaseCheck.checked)
+    funcArr.push(generateUpperCase);
+  if(lowercaseCheck.checked)
+    funcArr.push(generateLowerCase);
+  if(numbersCheck.checked)
+    funcArr.push(generateRandomNumber);
+  if(symbolsCheck.checked)
+    funcArr.push(generateSymbol);
+  //compulsary addition
+  for(let i=0;i<funcArr.length;i++){
+    password+=funcArr[i]();
+  }
+  console.log("compulusry");
+  //remaing 
+  for(let i=0;i<passwordLength-funcArr.length;i++){
+    let randIndex=getRndInteger(0,funcArr.length);
+    password+=funcArr[randIndex]();
+  }
+  console.log("remaing");
+
+  //suffle password
+  password=shufflePassword(Array.from(password));
+  console.log("shuffle");
+
+  //show in UI
+  passwordDisplay.value=password;
+  console.log("ui");
+  //calculate password strength
+  calcStrength();
+
+})
